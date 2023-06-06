fe496f88ede27f0f05ae74aefbfee2fd1c1df2d4
Cassiane Almeida <cassiane.almeida@estudante.ifro.edu.br>
2023-06-06 14:32:20 -0400

    first commit

---
+++ terreno/Program.cs
@@ -0,0 +1,5 @@
+﻿Terreno t = new Terreno(6, 6);
+
+Console.WriteLine($"A area do terreno é: {t.AreaTerreno()}");
+
+Console.WriteLine($"\nO valor estimado do terreno é: {t.ValorEstimado(100)}");
---
+++ terreno/Terreno.cs
@@ -0,0 +1,46 @@
+﻿public class Terreno
+{
+    //Atributos
+    private double largura;
+    private double comprimento;
+
+
+    //Metodo Get e Set
+    public double GetLargura()
+    {
+        return this.largura;
+    }
+    public void SetLargura(double largura)
+    {
+        this.largura = largura;
+    }
+
+
+    //Metodo Get e Set
+    public double GetComprimento()
+    {
+        return this.comprimento;
+    }
+    public void SetComprimento(double comprimento)
+    {
+        this.comprimento = comprimento;
+    }
+
+    //Metodo construtor
+    public Terreno(double largura, double comprimento)
+    {
+        this.largura = largura;
+        this.comprimento = comprimento;
+    }
+
+    public double AreaTerreno()
+    {
+        double area = largura * comprimento;
+        return area;
+    }
+
+    public double ValorEstimado(double metro)
+    {
+        return metro * AreaTerreno();
+    }
+}
